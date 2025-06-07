585e1d440023e4a4389b5d2943e0815c8db6af37
G Naga Mohan <nagamohan.giddaluri@thezennialpro.com>
2025-06-08 01:23:57 +0530

    Added basic structure files

---
+++ job_applicant_cli.py
@@ -0,0 +1,85 @@
+import json
+import logging
+from datetime  import datetime
+from functools import wraps
+
+logging.basicConfig (
+    filename = 'applicant_app.log',
+    level = logging.INFO,
+    format = '%(asctime)s - %(levelname)s - %(message)s'
+)
+
+logging = logging.getLogger(__name__)
+
+def handle_exceptions(func):
+    @wraps(func)
+    def wrapper (*args, **kwargs):
+        try:
+            logging.info(f"Executing: {func.__name__}")
+            return func(*args, **kwargs)
+        except Exception as e:
+            logging.exception(f"Error while Executing: {func.__name__} : {e}")
+    return wrapper
+class Project:
+    def __init__(self, email, name,status):
+        self.email = email
+        self.name = name
+        self.status = status
+
+class Applicant:
+    def __init__(self, name, email, role, location, skills, experience, expected_salary, status):
+        self.name =  name
+        self.email = email
+        self.role = role
+        self.location = location
+        self.skills =  skills
+        self.experience = experience
+        self.expected_salary = expected_salary
+        self.status = status
+
+# ***** Helper Methods
+@handle_exceptions
+def load_applicants(filepath):
+    with open (filepath, 'r') as f:
+        applicants  = json.load(f)
+        return [Applicant(**a) for a in  applicants]
+    
+@handle_exceptions
+def save_applicants(filepath, applicants):
+    with open(filepath, 'w') as f:
+        json.dump([a.__dict__ for a in applicants], f, indent=4)
+
+
+def showcli():
+    while True:
+        print("\n=== Job Applicant CLI ===")
+        print("1. Add New Applicant")
+        print("2. View Applicant by Email/Name")
+        print("3. Update Applicant Status")
+        print("4. Delete Applicant")
+        print("5. Filter Applicant by Role")
+        print("6. Filter Applicant by Location")
+        print("7. Filter Applicant by Skill")
+        print("8. Filter Applicant by Experience Range")
+        print("9. Filter Applicant by Expected Salary Range")
+        print("10. Filter Applicant by Application Status")
+        print("11. Count Applicants per Role")
+        print("12. Count Applicants per Location")
+        print("13. Count Applicants per Skill")
+        print("14. Avg Experience per Role")
+        print("15. Avg Expected Salary per Role")
+        print("16. Exit")
+
+
+        choice = input("Please Select an Option : ")
+            
+
+
+if __name__ == "__main__":
+    filepath = "job_applicants.json"
+    applicants = load_applicants(filepath=filepath)
+
+    if applicants:
+        showcli()
+    else:
+        print ("No Applicants found or file is missing")
